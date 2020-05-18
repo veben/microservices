0da897d4f79e8e8a67e5a5cbc7b82e2d95234c7b
veben <benoit.veyriere@gmail.com>
2020-05-18 17:28:32 +0200

    Migrated to Spring boot 2.3.0.RELEASE + refactored layered jars part + cleaned pom.xml

--- developer-information/src/main/java/com/veben/microservices/developerinformation/ext/rest/BaseController.java
+++ developer-information/src/main/java/com/veben/microservices/developerinformation/ext/rest/BaseController.java
@@ -4,7 +4,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 
 @CrossOrigin
 public abstract class BaseController {
-    private BaseController() {}
 
     static final String BASE_URI = "/api/developer-information/v1";
 }
